ab264bef0ecb9f9053ffa824ddbd19b426f4db54
jjcauchy-afk <jjcauchy@gmail.com>
2026-02-22 13:36:21 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
+import streamlit as st
 
+# Use st.write for a simple text message
+st.write("Hello World!")
+
+# Or use st.title for a larger, bold header
+st.title("👋 Hello Streamlit!")
+
+name = st.text_input("Enter your name:")
+
+if st.button("Submit"):
+    st.success(f"Hello, {name}!")
